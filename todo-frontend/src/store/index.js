b9e4a32683fcc1eee3385ecf0dc49d46fb9163ea
Filip Gogic <filipgogic95@gmail.com>
2020-09-11 01:25:09 +0200

    zavrseno

--- todo-frontend/src/store/index.js
+++ todo-frontend/src/store/index.js
@@ -4,7 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {baseUrl: 'http://localhost/todofullstack/todo-backend'},
   mutations: {},
   actions: {},
   modules: {}
---
+++ todo-frontend/src/views/ListView.vue
@@ -0,0 +1,23 @@
+<template>
+ 
+  <div>
+
+    <span></span>
+    <Navbar />
+    <List />
+  </div>
+</template>
+<script>
+import Navbar from "@/components/Navbar.vue";
+import List from "@/components/List.vue";
+export default {
+    components: {
+    Navbar,
+    List
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
